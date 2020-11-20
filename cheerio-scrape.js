05598dffac53054ae14f96478a9cb712c9f2e346
Amruta789 <amruta.jandhy@gmail.com>
2020-11-20 16:19:34 +0530

    Started website

--- cheerio-scrape.js
+++ cheerio-scrape.js
@@ -1,31 +1,18 @@
 const cheerio= require('cheerio');
 const fetch = require('node-fetch');
 
-function checkStatus(res) {
-    if (res.ok) { // res.status >= 200 && res.status < 300
-        return res.text();
-    } else {
-        console.log("bad");
-        throw new Error(res.statusText);
-    }
-}
+let place='Mumbai';
 
-
-fetch('https://www.npmjs.com/package/node-fetch/')
-    // .then(checkStatus)
-    
-    .then((res)=>{ 
+fetch(`https://www.weather-forecast.com/locations/${place}/forecasts/latest`)
+    .then(async(res)=>{ 
         if(res.ok){       
-            let $=cheerio.load(res);
-            console.log(res);
-            let siteData = $('#readme > p:nth-child(8)');
-            console.log(siteData);
+            let $=cheerio.load(await res.text());
+            let siteData = $('body > main > section:nth-child(3) > div > div > div.b-forecast__overflow > div > table > thead > tr.b-forecast__table-description.b-forecast__hide-for-small.days-summaries.js-day-summary');
+            console.log(siteData.text());            
+            siteData = $('body > main > section:nth-child(14) > div.small-12.large-8.columns > div > table > tbody');
             console.log(siteData.text());
-            console.log(siteData.html());
-            return res.text();
         }else{
             throw new Error(res.statusText);
         }
-    }) 
-    .then(body => console.log(body))       
+    })
     .catch(error => console.log(error))
